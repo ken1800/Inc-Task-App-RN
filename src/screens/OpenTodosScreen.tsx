9afa595ffcc2bb72d6f33b481e2e8477b3330310
Kenneth Irungu <kennethirungu1800@gmail.com>
2024-03-15 19:50:07 +0300

    chore/minor fix-ups

--- src/screens/OpenTodosScreen.tsx
+++ src/screens/OpenTodosScreen.tsx
@@ -5,30 +5,27 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import palette from '../../palette'
 import TodoList from '../components/TodoList'
-import { useNavigation } from '@react-navigation/core'
+import { NavigationProp } from '@react-navigation/native'
 
-function OpenTodosScreen() {
-  const navigation = useNavigation<any>()
+interface Props {
+  navigation: NavigationProp<any>
+}
 
+function OpenTodosScreen({ navigation }: Props) {
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: '2%' }}>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
         <Text variant="headlineMedium" style={styles.header}>
           Hi, welcome back!
         </Text>
         <Icon name='emoticon-happy-outline' size={100} color={palette.defaultTheme} />
       </View>
       <TodoList />
-      <View style={{ justifyContent: 'flex-end', alignItems: 'flex-end', marginBottom: '1%', marginRight: '5%' , marginTop:4}}>
+      <View style={styles.fabContainer}>
         <FAB
           icon="plus"
           color='white'
-          style={{
-            position: 'absolute',
-            bottom: 70,
-            right: 0,
-            backgroundColor: palette.defaultTheme,
-          }}
+          style={styles.fab}
           onPress={() => {
             navigation.navigate('Create Task')
           }}
@@ -41,11 +38,33 @@ function OpenTodosScreen() {
 export default OpenTodosScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '2%',
+  },
   header: {
     marginBottom: 10,
     paddingTop: 30,
     paddingHorizontal: 35,
     textAlign: 'center',
     color: palette.defaultTheme,
+  },
+  fabContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginBottom: '1%',
+    marginRight: '5%',
+    marginTop: 4,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: 70,
+    right: 0,
+    backgroundColor: palette.defaultTheme,
   },
 })
